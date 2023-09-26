08ce75cb2424aff9623d261b28a69107ff8f1246
Miguel Negron <manegron@redborder.com>
2023-09-26 23:04:32 +0100

    Match metadata version with tag

--- resources/metadata.rb
+++ resources/metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'jjprieto@redborder.com'
 license          'All rights reserved'
 description      'Installs/Configures cookbook-pmacct'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.1'
+version          '0.0.3'
