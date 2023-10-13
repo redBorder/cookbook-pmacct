eb6061134f6121a1e7c48a2f2e153bce92dae221
Miguel Negron <manegron@redborder.com>
2023-10-13 10:31:01 +0100

    Fix errors

--- resources/resources/config.rb
+++ resources/resources/config.rb
@@ -8,10 +8,10 @@ default_action :add
 
 attribute :user, :kind_of => String, :default => "pmacct"
 attribute :cdomain, :kind_of => String, :default => "redborder.cluster"
-attribute :sensors, :kind_of => Hash, :default => []
-attribute :kafka_hosts, :kind_of => Array, :default => "kafka.service"
+attribute :sensors, :kind_of => Array, :default => []
+attribute :kafka_hosts, :kind_of => Array, :default => ["kafka.service"]
 attribute :kafka_topic, :kind_of => String, :default => "sflow"
 attribute :kafka_broker_port, :kind_of => Integer, :default => 9092
-attribute :geo_country, :kind_of => Array, :default => "/usr/share/GeoIP/GeoLiteCountry.dat"
+attribute :geo_country, :kind_of => >String, :default => "/usr/share/GeoIP/GeoLiteCountry.dat"
 
 
