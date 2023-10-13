90a628fa9007f7464d05bef189eb47dffccd0e42
Miguel Negron <manegron@redborder.com>
2023-10-13 23:01:07 +0100

    Fix sensors is hash

--- resources/resources/config.rb
+++ resources/resources/config.rb
@@ -8,7 +8,7 @@ default_action :add
 
 attribute :user, :kind_of => String, :default => "pmacct"
 attribute :cdomain, :kind_of => String, :default => "redborder.cluster"
-attribute :sensors, :kind_of => Array, :default => []
+attribute :sensors, :kind_of => Hash, :default => {}
 attribute :kafka_hosts, :kind_of => Array, :default => ["kafka.service"]
 attribute :kafka_topic, :kind_of => String, :default => "sflow"
 attribute :kafka_broker_port, :kind_of => Integer, :default => 9092
