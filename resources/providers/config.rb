9475d14c40d353eb204778d483859cf71ef88732
Miguel Negron <manegron@redborder.com>
2023-10-15 01:25:44 +0100

    Create user with command

--- resources/providers/config.rb
+++ resources/providers/config.rb
@@ -21,9 +21,10 @@ action :add do
       flush_cache [:before]
     end
 
-    user user do
-      action :create
-      system true
+    execute "create_user" do
+      command "/usr/sbin/useradd -r #{user}"
+      ignore_failure true
+      not_if "getent passwd #{user}"
     end
 
     flow_nodes = []
