a3cdc680d2fed21e5a24aba945d9c3710ddc5dfa
Miguel Negron <manegron@redborder.com>
2024-06-01 14:35:27 +0100

    lint resources

--- resources/resources/config.rb
+++ resources/resources/config.rb
@@ -1,17 +1,13 @@
-# Cookbook Name:: pmacct
-#
+# Cookbook:: pmacct
 # Resource:: config
-#
 
 actions :add, :remove, :register, :deregister
 default_action :add
 
-attribute :user, :kind_of => String, :default => "pmacct"
-attribute :cdomain, :kind_of => String, :default => "redborder.cluster"
-attribute :sensors, :kind_of => Hash, :default => {}
-attribute :kafka_hosts, :kind_of => Array, :default => ["127.0.0.1"]
-attribute :kafka_topic, :kind_of => String, :default => "sflow"
-attribute :kafka_broker_port, :kind_of => Integer, :default => 9092
-attribute :geo_country, :kind_of => String, :default => "/usr/share/GeoIP/GeoLiteCountry.dat"
-
-
+attribute :user, kind_of: String, default: 'pmacct'
+attribute :cdomain, kind_of: String, default: 'redborder.cluster'
+attribute :sensors, kind_of: Hash, default: {}
+attribute :kafka_hosts, kind_of: Array, default: ['127.0.0.1']
+attribute :kafka_topic, kind_of: String, default: 'sflow'
+attribute :kafka_broker_port, kind_of: Integer, default: 9092
+attribute :geo_country, kind_of: String, default: '/usr/share/GeoIP/GeoLiteCountry.dat'
